c139adee679a37356d8d7ca69a41a30e97200199
Freddie Fujiwara <github@ze.gs>
2013-01-15 23:01:13 +0900

    get param

--- app/index.php
+++ app/index.php
@@ -3,5 +3,6 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $yamaki = new Yamaki();
 $yamaki -> get("/yamaki/:ichiban/:niban",function($route,$input){
-   echo __FILE__.":".__LINE__."\n";
+   echo "ichiban:".$route -> param("ichiban")."\n";
+   echo "niban:".$route -> param("niban")."\n";
 });
