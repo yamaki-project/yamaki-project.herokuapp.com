bd3d3950c000614edf43594b44927a105a33a3d2
Freddie Fujiwara <github@ze.gs>
2013-01-15 22:35:36 +0900

    bug fixed

--- app/index.php
+++ app/index.php
@@ -2,6 +2,6 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 $yamaki = new Yamaki();
-$yamaki-> get("/yamaki/:ichiban/:niban",function($route,$input){
+$yamaki -> get("/yamaki/:ichiban/:niban",function($route,$input){
    echo __FILE__.":".__LINE__."\n";
-);
+});
