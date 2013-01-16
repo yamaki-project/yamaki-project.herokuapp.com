30c42d80499d4501d8e1a7356d60a897acf2de14
Freddie Fujiwara <github@ze.gs>
2013-01-16 10:40:42 +0900

    adopt 0.0.3

--- app/index.php
+++ app/index.php
@@ -1,8 +1,7 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 
-$yamaki = new Yamaki();
-$yamaki -> get("/yamaki/:ichiban/:niban",function($route,$input){
+Yamaki::get("/yamaki/:ichiban/:niban",function($route,$input){
    echo "ichiban:".$route -> param("ichiban")."\n";
    echo "niban:".$route -> param("niban")."\n";
 });
